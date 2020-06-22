427c7a4ca9b813110e4eccf71d278fbdf9992e71
Alex Dong <alexwdong@gmail.com>
2020-06-22 17:22:00 -0400

    Changes to Basic CNN arch

--- src/Basic_CNN_Architecture.py
+++ src/Basic_CNN_Architecture.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 class BasicCNN_128x128(nn.Module):
     def __init__(self,num_classes):
-        super(BasicCNN, self).__init__()
+        super(BasicCNN_128x128, self).__init__()
          
         in_channels= 3  
         conv1_out_channels= 8 
@@ -11,23 +11,26 @@ class BasicCNN_128x128(nn.Module):
         lin1_out_channels= 64
         kernel_size = 5
         stride = 2
+        padding = 2
         dropout = None
         activation = nn.ReLU(inplace = False)
         
         self.conv1 = nn.Conv2d(
-                    in_channels=in_channels, 
-                    out_channels=conv1_out_channels,
-                    kernel_size=kernel_size, 
-                    stride=stride,
-                    padding_mode='zeros'
-                    )
+            in_channels=in_channels, 
+            out_channels=conv1_out_channels,
+            kernel_size=kernel_size, 
+            stride=stride,
+            padding=padding,
+            padding_mode='zeros'
+            )
         self.conv2 = nn.Conv2d(
-                    in_channels=conv1_out_channels, 
-                    out_channels=conv2_out_channels,
-                    kernel_size=kernel_size, 
-                    stride=stride,
-                    padding_mode='zeros'
-                    )
+            in_channels=conv1_out_channels, 
+            out_channels=conv2_out_channels,
+            kernel_size=kernel_size, 
+            stride=stride,
+            padding=padding,
+            padding_mode='zeros'
+            )
         self.pool1 = nn.MaxPool2d(2)
         self.pool2 = nn.MaxPool2d(2)
         
@@ -48,10 +51,10 @@ class BasicCNN_128x128(nn.Module):
             self.dropout2 = None
         
         self.activation = activation
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, image):
-        #print(0,x.size())
+        #print(0,image.size())
         out = self.conv1(image)
         #print(1,out.size())
         out = self.activation(out)
@@ -73,15 +76,14 @@ class BasicCNN_128x128(nn.Module):
         out = self.fc1(out)
         out = self.activation(out)
         out = self.fc2(out)
+        #print(8,out.size())
         out = self.softmax(out)
-        return out
-    
-    
+        return out 
     
 
 class BasicCNN_w_features_128x128(nn.Module):
     def __init__(self,num_classes):
-        super(BasicCNN, self).__init__()
+        super(BasicCNN_w_features_128x128, self).__init__()
          
         in_channels= 3  
         conv1_out_channels= 8 
