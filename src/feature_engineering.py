6f185eca8faab9e702ec5dd3416a0388e4943233
Alex Dong <alexwdong@gmail.com>
2020-06-26 18:39:47 -0400

    added comments to feature_engineering

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -4,6 +4,22 @@ from sklearn.preprocessing import OneHotEncoder
 
 
 def prepare_spectral_clustering_features(X, n_clusters):
+    '''
+    Inputs:
+        X: data matrix or dataframe. Each data instance is expected to be a row
+            in the matrix or dataframe
+        n_cluster: number of clusters
+    Outputs:
+        return: returns a one-hot vector encoding of the clusters. For example, if 
+        there are 6 data points, belonging to clusters [0,0,1,1,2,2], then the return
+        array will be
+        [1,0,0]
+        [1,0,0]
+        [0,1,0]
+        [0,1,0]
+        [0,0,1]
+        [0,0,1]
+    '''
     cluster_model = SpectralClustering(n_clusters=n_clusters,
                                        n_init = 10,
                                        assign_labels="discretize",
@@ -17,6 +33,15 @@ def prepare_spectral_clustering_features(X, n_clusters):
     return one_hot_vec
 
 def prepare_eigen_component_features(images_list,eig_vecs,):
+    '''
+    Inputs:
+        images_list: list of images (images are ndarrays, usually of 2 dimensions (e.g 128x128))
+        eig_vecs: an 2d array of eigenvectors, where each eigenvector is a column
+    Outputs:
+        return: returns a feature matrix, where the rows of the feature matrix correspond to the 
+        features of images. First image is the first row of the matrix, last image is the last row.
+        The features are the components of the eigenvectors (which were passed in the input 'eig_vecs')
+    '''
     n_components = eig_vecs.shape[1]
     feature_matrix = np.zeros(len(images_list),n_components)
     for image in images_list:
